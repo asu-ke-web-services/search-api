07bb2b94c135a5502ab1a0ca2037457173daff1c
Ryan Raub <ryan.raub@asu.edu>
2015-10-09 13:17:59 -0700

    Meet coding standards (added comments and whitespace)

--- src/models/search-result.php
+++ src/models/search-result.php
@@ -2,5 +2,8 @@
 
 namespace SearchApi\Models;
 
+/**
+ * Class SearchResult - This will be the class that defines the response to a search
+ */
 class SearchResult {
 }
--- src/models/synonym.php
+++ src/models/synonym.php
@@ -2,5 +2,8 @@
 
 namespace SearchApi\Models;
 
+/**
+ * Class Synonym - This will be the class that defines how synonyms are structured
+ */
 class Synonym {
 }
