041e2669fd11a3a005e16a7b47ff0060a46d7b84
Ken Price <kaprice1@asu.edu>
2015-10-27 10:43:06 -0700

    Have SearchEngine use SolrSearch to get search results

--- src/search-api/search-engine.php
+++ src/search-api/search-engine.php
@@ -2,6 +2,7 @@
 
 namespace SearchApi;
 use SearchApi\Models as Models;
+use SearchApi\Providers as Providers;
 
 /**
  * Class SearchEngine - This is the main entry point into this application, it takes search requests and
@@ -9,15 +10,20 @@ use SearchApi\Models as Models;
  */
 class SearchEngine {
 
+  private $searchProvider;
+
   public function handle_request( Models\SearchRequest $request ) {
     if ( empty( $request ) ) {
       // handle an empty request
       return new Models\SearchResult();
     }
+
+    $searchProvider = new Providers\SolrSearch();
+
     // do stuff with $request
     $response = new Models\SearchResult();
-    $response->results = array( 'foo', 'test' );
-    $response->count = 2;
+    $response->results = $searchProvider->query($request->text);
+    $response->count = count($response->results);
     return $response;
   }
 }
