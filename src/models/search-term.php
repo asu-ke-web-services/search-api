5abfaabe995b5317b54f975e948dd5a4d9c0a2f2
Ken Price <kaprice1@asu.edu>
2015-11-02 19:57:49 -0800

    Add SearchTerm model.

---
+++ src/models/search-term.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SearchApi\Models;
+
+/**
+ * Class SearchTerm - A search term is a basic unit that will be used to build a query for
+ * Search providers.
+ */
+class SearchTerm {
+  public $value;          // (string)     This will be a keyword from NerTagger, location from ReverseGeocoder, etc.
+  public $category;       // (string)     Category as string: 'location', 'person', 'organization', etc.
+  public $related;        // (string[])   Array of related keywords
+  public $count;          // (int)        Occurences of keyword
+  public $isUserInput;    // (bool)       TRUE if term is a user-specified keyword, FALSE if it is not
+}
