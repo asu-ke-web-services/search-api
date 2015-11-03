62516c05a7c017082b04c9bab6189f0ffed2b683
Ken Price <kaprice1@asu.edu>
2015-11-02 21:08:37 -0800

    Add phpdoc-style comments to SearchTerm

--- src/models/search-term.php
+++ src/models/search-term.php
@@ -5,13 +5,19 @@ namespace SearchApi\Models;
 /**
  * Class SearchTerm - A search term is a basic unit that will be used to build a query for
  * Search providers.
+ *
+ * @var $value       string      This will be a keyword from NerTagger, location from ReverseGeocoder, etc.
+ * @var $category    string      'location', 'person', 'organization', etc.
+ * @var $related     string[]    Array of related keywords
+ * @var $count       int         Occurences of keyword
+ * @var $isUserInput bool        TRUE if term is a user-specified keyword, FALSE if it is not
  */
 class SearchTerm {
-  public $value;          // (string)     This will be a keyword from NerTagger, location from ReverseGeocoder, etc.
-  public $category;       // (string)     Category as string: 'location', 'person', 'organization', etc.
-  public $related;        // (string[])   Array of related keywords
-  public $count;          // (int)        Occurences of keyword
-  public $isUserInput;    // (bool)       TRUE if term is a user-specified keyword, FALSE if it is not
+  public $value;
+  public $category;
+  public $related;
+  public $count;
+  public $isUserInput;
 
   function __construct( $value, $category, $related, $count, $isUserInput ) {
     $this->value = $value;
