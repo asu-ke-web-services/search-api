d8ee6eccdd5f83aea59c69a1027caf8072f72872
Ken Price <kaprice1@asu.edu>
2015-11-03 15:33:49 -0800

    Fix phpdoc comment

--- src/services/search.php
+++ src/services/search.php
@@ -3,9 +3,9 @@
 namespace SearchApi\Services;
 
 /**
- * Class Search - Should make a search query and return the search results as an object
+ * Interface Search - Should make a search query and return the search results as an object
  *
- * @method SearchResult query (SearchTerm[]|null $keywords, SearchApi\Models\SearchOptions|null)
+ * @method SearchResult query (SearchTerm[]|null $keywords, SearchApi\Models\SearchOptions|null $options)
  *   Make a search query with options for pagination, sorting, etc.
  */
 interface Search {
