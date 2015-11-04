dd048d6b95c8b112de113604e9139825ec2aae9a
Ken Price <kaprice1@asu.edu>
2015-11-03 19:18:10 -0800

    Rewrite according to SolrSearch specs.

--- src/providers/solr-search.php
+++ src/providers/solr-search.php
@@ -7,12 +7,31 @@ use SearchApi\Models\SearchTerm;
 use SearchApi\Models\SearchResult;
 use SearchApi\Models\SearchOptions;
 
+use SearchApi\Builders\QueryBuilder;
+use SearchApi\Builders\SolrQueryBuilder;
+
 /**
  * Class SolrSearch - This is a placeholder class that will be an interface to a Solr Search Index
  *
- * @method SearchResult query (SearchTerm[]|null $keywords, SearchOptions|null) Interface defined in Services\Search
+ * @var QueryBuilder queryBuilder Obj with implementation of QueryBuilder constructing Solr-specific query string.
+ * @method SearchResult query (SearchTerm[]|null $keywords, SearchOptions|null options) Interface defined in Services\Search
  */
 class SolrSearch implements Search {
+
+  private $queryBuilder;
+
+  /**
+   * Constructor
+   *
+   * @param queryBuilder QueryBuilder Should be able to inject dependency to use any solr-specific impl. of QueryBuilder
+   */
+  function __construct( QueryBuilder $queryBuilder = null ) {
+    if ( $queryBuilder ) {
+      $this->queryBuilder = $queryBuilder;
+    } else {
+      $this->queryBuilder = new SolrQueryBuilder();
+    }
+  }
 
   function query( $keywords, $options = null ) {
     // This is just a placeholder
@@ -20,16 +39,10 @@ class SolrSearch implements Search {
       return null;
     }
 
-    $results = array();
-    $searchResult = new SearchResult();
+    $queryString = $this->queryBuilder->build_query( $keywords, $options );
 
-    foreach ( $keywords as $term ) {
-      array_push( $results, $term );
-    }
+    // do something with query string
 
-    $searchResult->results = $results;
-    $searchResult->count = count( $results );
-
-    return $searchResult;
+    return new SearchResult();
   }
 }
