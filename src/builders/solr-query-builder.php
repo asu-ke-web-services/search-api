4f5fcd5ef3651a14e065dc2986cfd13fc8c5d63c
Ivan Montiel <idmontie@asu.edu>
2015-11-05 13:57:30 -0700

    Fix documentation for query builder

--- src/builders/solr-query-builder.php
+++ src/builders/solr-query-builder.php
@@ -5,12 +5,12 @@ namespace SearchApi\Builders;
 use SearchApi\Builders\QueryBuilder;
 
 /**
- * Class SolrQueryBuilder - Responsible for building a query string for SolrSearch.
- *
- * @method string build_query (SearchTerm[]|null $keywords, SearchApi\Models\SearchOptions|null)
- *   Build a query string from provided search terms and search options
+ * Class SolrQueryBuilder - Responsible for building a query string for SolrSearch
  */
 class SolrQueryBuilder implements QueryBuilder {
+  /**
+   * Build a query string from provided search terms and search options
+   */
   function build( $keywords, $options = null ) {
     return null;
   }
