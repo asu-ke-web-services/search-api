fef6d71a63bd536b3ed82afad6400b34ed54d185
TahBaconSandwich <ericds92@gmail.com>
2015-11-05 12:54:38 -0700

    Update google-reverse-geocoder
    adds mock functions for calling google's reverse geocoder
    	-function to reverse geocode with latitude and longitude
    	-function to reverse geocode with place id

--- src/providers/google-reverse-geocoder.php
+++ src/providers/google-reverse-geocoder.php
@@ -2,11 +2,27 @@
 
 namespace SearchApi\Providers;
 
-use SearchApi\Services\ReverseGeocoder;
+use SearchApi\Models as Models;
+use SearchApi\Test as Test;
+use SearchApi\Support as Support;
+use SearchApi\Services\ReverseGeocoder as ReverseGeocoder;
 
 /**
- * Class GoogleReverseGeocoder - Given a location name find a point or polygon that can represent that
+ * Class GoogleReverseGeocoder - Given a latlin or place id to find a point or polygon that can represent that
  * location via GoogleMaps.
  */
 class GoogleReverseGeocoder implements ReverseGeocoder {
+  public function reverse_geocoding_with_latlin( Models\GeoCoordinate $geo_coordinate, $key ) {
+  	$parser = new Support\Geo_Parser();
+
+    $geocoding_results = Test\Support\Google_Geocoder_Mock::reverse_geocoding( $geo_coordinate, $key );
+    return $parser->reverse_geocoder_parser( $geocoding_results );
+  }
+
+  public function reverse_geocoding_with_place_id( $place_id, $key ) {
+  	$parser = new Support\Geo_Parser();
+
+  	$geocoding_results = Test\Support\Google_Geocoder_Mock::reverse_geocoding( $place_id, $key );
+  	return $parser->reverse_geocoder_parser( $geocoding_results );
+  }
 }
