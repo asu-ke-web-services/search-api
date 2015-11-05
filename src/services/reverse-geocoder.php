c1eb8e477283e7aeaed2313a23876b5c63df3ab0
TahBaconSandwich <ericds92@gmail.com>
2015-11-05 12:50:57 -0700

    Update ReverseGeocoder interface
    	-add function reverse_geocoding_with_latlin (latitude and longitude)
    	-add fucntion reverse_geocoding_with_place_id

--- src/services/reverse-geocoder.php
+++ src/services/reverse-geocoder.php
@@ -2,5 +2,9 @@
 
 namespace SearchApi\Services;
 
+use SearchApi\Models as Models;
+
 interface ReverseGeocoder {
+  function reverse_geocoding_with_latlin( Models\GeoCoordinate $geo_coordinate, $key );
+  function reverse_geocoding_with_place_id( $place_id, $key );
 }
