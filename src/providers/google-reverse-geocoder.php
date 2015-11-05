ecf5a9056c3936ee35d6153e5e57756e1bf933ea
TahBaconSandwich <ericds92@gmail.com>
2015-11-05 14:48:05 -0700

    Update GoogleReverseGeoCoder for the new mock location

--- src/providers/google-reverse-geocoder.php
+++ src/providers/google-reverse-geocoder.php
@@ -3,7 +3,7 @@
 namespace SearchApi\Providers;
 
 use SearchApi\Models as Models;
-use SearchApi\Test as Test;
+use SearchApi\Test\Support\Mocks as Mocks;
 use SearchApi\Support as Support;
 use SearchApi\Services\ReverseGeocoder as ReverseGeocoder;
 
@@ -15,14 +15,14 @@ class GoogleReverseGeocoder implements ReverseGeocoder {
   public function reverse_geocoding_with_latlng( Models\GeoCoordinate $geo_coordinate, $key ) {
   	$parser = new Support\Geo_Parser();
 
-    $geocoding_results = Test\Support\Google_Geocoder_Mock::reverse_geocoding( $geo_coordinate, $key );
+    $geocoding_results = Mocks\Google_Geocoder_Mock::reverse_geocoding( $geo_coordinate, $key );
     return $parser->reverse_geocoder_parser( $geocoding_results );
   }
 
   public function reverse_geocoding_with_place_id( $place_id, $key ) {
   	$parser = new Support\Geo_Parser();
 
-  	$geocoding_results = Test\Support\Google_Geocoder_Mock::reverse_geocoding( $place_id, $key );
+  	$geocoding_results = Mocks\Google_Geocoder_Mock::reverse_geocoding( $place_id, $key );
   	return $parser->reverse_geocoder_parser( $geocoding_results );
   }
 }
