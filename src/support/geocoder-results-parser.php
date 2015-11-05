a2e3682e52118a0e25e8cfa947bcdf72ee5bed1f
TahBaconSandwich <ericds92@gmail.com>
2015-11-05 12:55:46 -0700

    Add geocoder-results-parser
    class that will be used to parse the returned values from the geocoder
    	-used for both reverse geocoding and geocoding

---
+++ src/support/geocoder-results-parser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SearchApi\Support;
+
+/**
+ * Class Geo_Parser - Parses the returned result from a Geocoder
+ */
+class Geo_Parser {
+  public function reverse_geocoder_parser( $geocoder_results ) {
+    // add code to parse either xml or json results returned by google
+
+    // for now: pretends to parse the results and return address
+  	return '277 Bedford Avenue, Brooklyn, NY 11211, USA';
+  }
+}
