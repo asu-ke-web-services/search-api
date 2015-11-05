689753fc67eae7942f192b621dd938b3d6a64d49
TahBaconSandwich <ericds92@gmail.com>
2015-11-05 11:45:19 -0700

    Fix whitespace issues

--- test/spec/Providers/GoogleReverseGeocoderSpec.php
+++ test/spec/Providers/GoogleReverseGeocoderSpec.php
@@ -15,15 +15,15 @@ class GoogleReverseGeocoderSpec extends ObjectBehavior {
   function it_is_initializable() {
     $this->shouldHaveType( 'SearchApi\Providers\GoogleReverseGeocoder' );
   }
-  
+
   function it_should_return_a_result() {
   	$geo_cordinate = new Models\GeoCoordinate( 123, 456 );
   	$key = 'geokey';
   	$this->reverse_geocoding_with_latlin( $geo_cordinate, $key );
   }
-  
+
   function it_should_return_invalid_key() {
-  	$this->reverse_geocoding_with_latlin( 'place_id_var', 'bad_key_test_var')
-  	  ->shouldBeCalled()->willReturn( 'Invalid Key' );
+  	$this->reverse_geocoding_with_latlin( 'place_id_var', 'bad_key_test_var' )
+    ->shouldBeCalled()->willReturn( 'Invalid Key' );
   }
 }
