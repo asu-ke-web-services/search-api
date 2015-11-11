684930f595dc165bf24a1a106925f3761b41b474
TahBaconSandwich <ericds92@gmail.com>
2015-11-11 16:09:47 -0700

    Change GoogleReverseGeocoderSpec
    	-add null test for api_key
    		-found out the key is optional
    		-have test if going to use key
    	-change invalid key test
    		-check for error thrown

--- test/spec/SearchApi/Providers/GoogleReverseGeocoderSpec.php
+++ test/spec/SearchApi/Providers/GoogleReverseGeocoderSpec.php
@@ -16,18 +16,22 @@ class GoogleReverseGeocoderSpec extends ObjectBehavior {
   function it_is_initializable() {
     $this->shouldHaveType( 'SearchApi\Providers\GoogleReverseGeocoder' );
   }
+  
+  function it_should_have_key_null() {
+  	$this->api_key->shouldBe( NULL );
+  }
 
   function it_should_return_a_result() {
   	$this->api_key = 'geokey';
   	$geo_cordinate = new Models\GeoCoordinate( 123, 456 );
   	$this->get_locations( $geo_cordinate )
   	->shouldReturn( '277 Bedford Avenue, Brooklyn, NY 11211, USA' );
-  }
+  }  
 
-  function it_should_return_invalid_key() {
-  	$this->api_key = 'bad_key_test';
+  function it_should_throw_invalid_key() {
+  	$this->beConstructedWith( 'bad_key_test' );
   	$geo_cordinate = new Models\GeoCoordinate( 123, 456 );
-  	$this->get_locations( $geo_cordinate )
-    ->shouldReturn( 'Invalid Key' );
+    $this->shouldThrow( new \InvalidArgumentException( 'Invalid Key' ) )
+    ->during( 'get_locations', array( $geo_cordinate ) );
   }
 }
