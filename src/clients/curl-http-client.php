2fd54fe39b53fa0f502bbccd29d8bdd6525192df
Ken Price <kaprice1@asu.edu>
2015-11-18 15:19:14 -0800

    Add HttpClient, a wrapper for http clients for simple http requests

---
+++ src/clients/curl-http-client.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SearchApi\Clients;
+
+/**
+ * Uses curl to make http requests.
+ */
+class CurlHttpClient implements HttpClient {
+  /**
+   * Simple GET request. Given a URL, return a string for the response.
+   */
+  private $curl;
+
+  function __construct() {
+    // create curl resource
+    $this->curl = curl_init();
+  }
+
+  function __destruct() {
+    curl_close( $this->curl );
+  }
+
+  function get( $url ) {
+    // Use curl to make GET request to SOLR server.
+    $ch = $this->curl;
+    curl_setopt( $ch, CURLOPT_URL, $url );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+    $response = curl_exec( $ch );
+
+    return $response;
+  }
+}
