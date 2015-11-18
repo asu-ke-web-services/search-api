303f11a09fd5af9f03fd41075e2166e5026e02c4
Ken Price <kaprice1@asu.edu>
2015-11-18 15:31:10 -0800

    Spec test for new behavior: uses an HttpClient to make GET requests

--- test/spec/SearchApi/Providers/SolrSearchSpec.php
+++ test/spec/SearchApi/Providers/SolrSearchSpec.php
@@ -6,6 +6,7 @@ use SearchApi;
 use SearchApi\Models;
 use SearchApi\Services\Search;
 use SearchApi\Builders\QueryBuilder;
+use SearchApi\Clients\HttpClient;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -32,10 +33,12 @@ class SolrSearchSpec extends ObjectBehavior {
     $result->shouldBeNull();
   }
 
-  function it_should_return_search_result_when_keywords_not_empty() {
-    $keywords = array( new Models\SearchTerm( 'test' ) );
-    $result = $this->query( $keywords );
+  function it_should_return_search_result_when_keywords_not_empty( QueryBuilder $queryBuilder, HttpClient $httpClient ) {
+    $queryBuilder->build( 'test', null )->willReturn( 'test2' );
+    $httpClient->get( Argument::any() )->willReturn( '{"responseHeader":{"response":{"numFound":1,"start":0,"docs":[{"id":"testid","author":"testauthor","date":"testdate","content":"testcontent"}]}}' );
 
+    $result = $this->query( 'test' );
     $result->shouldHaveType( 'SearchApi\Models\SearchResult' );
+    $result->count->shouldBe( 1 );
   }
 }
