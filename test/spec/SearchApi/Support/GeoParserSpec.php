b74a79faf17a7600ce9aadc891b9609bc98d3fd8
TahBaconSandwich <ericds92@gmail.com>
2015-11-17 21:16:50 -0700

    update GeoparserSpec
    -add new tests
    	-it_should_return_an_empty_array_when_passed_null
    	-it_should_return_an_empty_array_when_passed_empty_array
    	-it_should_return_an_empty_array_when_passed_bad_formatted_array
    	
    -rename test: it_should_return_an_empty_array
    	-to: it_should_return_an_empty_array_after_json_decoding_and_parsing

--- test/spec/SearchApi/Support/GeoParserSpec.php
+++ test/spec/SearchApi/Support/GeoParserSpec.php
@@ -16,21 +16,44 @@ class GeoParserSpec extends ObjectBehavior {
     $this->shouldHaveType( 'SearchApi\Support\GeoParser' );
   }
 
-  function it_should_return_an_empty_array() {
+  // reverse_geocoder_json_decoder tests
+  function it_should_throw_invalid_key() {
+  	$this->shouldThrow( new \Exception( 'Invalid Key' ) )
+  	->during( 'reverse_geocoder_json_decoder', array( '{"status" : "REQUEST_DENIED"}' ) );
+  }
+  
+  function it_should_throw_invalid_json_object() {
+  	$this->shouldThrow( new \Exception( 'Invalid Json Object' ) )
+  	->during( 'reverse_geocoder_json_decoder', array( '{bad_json : isbad}' ) );
+  }
+  // end of reverse_geocoder_json_decoder tests
+
+  // reverse_geocoder_parser tests
+  function it_should_return_an_empty_array_when_passed_null() {
+  	$result = $this->reverse_geocoder_parser( null );
+  	$result->shouldBeArray();
+  	$result->shouldHaveCount( 0 );
+  }
+
+  function it_should_return_an_empty_array_when_passed_empty_array() {
+  	$result = $this->reverse_geocoder_parser( array() );
+  	$result->shouldBeArray();
+  	$result->shouldHaveCount( 0 );
+  }
+
+  function it_should_return_an_empty_array_when_passed_bad_formatted_array() {
+  	$result = $this->reverse_geocoder_parser( array( "a", "b", "f" ) );
+  	$result->shouldBeArray();
+  	$result->shouldHaveCount( 0 );
+  }
+  // end of reverse_geocoder_parser tests
+
+  // combined reverse_geocoder_json_decoder and reverse_geocoder_parser tests
+  function it_should_return_an_empty_array_after_json_decoding_and_parsing() {
   	$result = $this->reverse_geocoder_json_decoder( '{"test":"test"}' );
   	$result = $this->reverse_geocoder_parser( $result );
   	$result->shouldBeArray();
   	$result->shouldHaveCount( 0 );
   }
-  	
-
-  function it_should_throw_invalid_key() {
-  	$this->shouldThrow( new \Exception( 'Invalid Key' ) )
-  	->during( 'reverse_geocoder_json_decoder', array( '{"status" : "REQUEST_DENIED"}' ) );
-  }
-
-  function it_should_throw_invalid_json_object() {
-  	$this->shouldThrow( new \Exception( 'Invalid Json Object' ) )
-  	->during( 'reverse_geocoder_json_decoder', array( '{bad_json : isbad}' ) );
-  }
+  // end of combined reverse_geocoder_json_decoder and reverse_geocoder_parser tests
 }
