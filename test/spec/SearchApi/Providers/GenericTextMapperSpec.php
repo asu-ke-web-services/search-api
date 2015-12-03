c106099aec42706c09a8585cbe88d5d6016af684
Thomas Rojchanakasetchai <trojcha1@gmail.com>
2015-12-03 08:08:15 -0700

    Update to test arrays

--- test/spec/SearchApi/Providers/GenericTextMapperSpec.php
+++ test/spec/SearchApi/Providers/GenericTextMapperSpec.php
@@ -13,21 +13,21 @@ use Prophecy\Argument;
  * GenericTextMapperSpec - Spec test for GenericTextMapper
  */
 class GenericTextMapperSpec extends ObjectBehavior {
-  function it_should_return_null_when_word_is_empty() {
+  function it_should_return_null_when_input_is_empty() {
     $result = $this->get_synonyms( null );
 
     $result->shouldBeNull();
   }
 
-  function it_should_return_a_synonymn_object_when_word_not_empty() {
-    $result = $this->get_synonyms( 'word' );
+  function it_should_return_a_countable_array_when_input_not_empty() {
+    $result = $this->get_synonyms( array( 'smelly', 'cat' ) );
 
-    $result->shouldHaveType( 'SearchApi\Models\Synonym' );
+    $result->shouldHaveCount( 2 );
   }
-  
-  function it_return_a_synonym_containing_synonyms() {
-    $result = $this->get_synonyms( 'word' );
 
-    $result->synonyms->shouldHaveCount( 3 );
+  function it_returns_an_array_of_type_synonym_containing_synonyms() {
+    $result = $this->get_synonyms( array( 'smelly', 'cat' ) );
+
+    $result[0]->synonyms->shouldNotBeNull();
   }
 }
