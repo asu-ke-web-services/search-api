bc1640b8ec486275be47cea252b8f4df303c079b
Ryan Raub <ryan.raub@gmail.com>
2015-12-03 13:53:00 -0700

    declared array

--- src/providers/ner-tagger.php
+++ src/providers/ner-tagger.php
@@ -45,6 +45,7 @@ class NerTagger implements Tagger {
       return null;
     }
 
+    $keywords = array();
     // This works properly, but does not handle 'relevance' yet
     foreach ( $tagger_results as $result ) {
       $keywords[] = new Keyword( $result[0], $result[1], 1.0 );
