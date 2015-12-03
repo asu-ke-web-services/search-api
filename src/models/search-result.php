2284130c94b094a4b344ecad1f0e7865f3e55e92
Jillian Karner <jillian.karner@gmail.com>
2015-12-03 09:31:49 -0700

    There should only be one model per class

--- src/models/search-result.php
+++ src/models/search-result.php
@@ -14,18 +14,3 @@ class SearchResult {
   public $count = 0;
   public $originalRequest;
 }
-
-/**
- * Class SearchResultItem - Defines a particular item in a search result.
- *
- * @var $id mixed Unique id of the document
- * @var $title string Title of the document
- * @var $author string[] Author(s) of the document
- * @var $date DateTime Publish date
- */
-class SearchResultItem {
-  public $id;
-  public $title;
-  public $author;
-  public $date;
-}
