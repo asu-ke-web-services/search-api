ebd303b1795194e1f66456e637020e4624f8a0c9
Thomas Rojchanakasetchai <trojcha1@gmail.com>
2015-12-03 08:04:09 -0700

    Update to handle arrays

--- src/providers/generic-mapper.php
+++ src/providers/generic-mapper.php
@@ -11,18 +11,23 @@ use SearchApi\Models\Synonym;
  */
 class GenericTextMapper implements TextMapper{
 
+  private $synonyms;
   private $synonym;
 
   // Placeholder function for testing
-  function get_synonyms( $word ) {
-    if ( $word === null ) {
+  function get_synonyms( $words ) {
+    if ( $words === null ) {
       return null;
     }
-    $synonym = new Synonym();
-    $synonym->word = $word;
-    // Use service to populate synonym array
-    $synonym->synonyms = array( 'synonym1', 'synonym2', 'synonym3' );
 
-    return $synonym;
+    foreach ( $words as &$word ) {
+      $synonym = new Synonym();
+      $synonym->word = $word;
+      // Use service to populate synonym array
+      $synonym->synonyms = array( 'synonym1', 'synonym2', 'synonym3' );
+      $synonyms[] = $synonym;
+    }
+
+    return $synonyms;
   }
 }
