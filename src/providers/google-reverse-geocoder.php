825ba6c046815f836c3dd9da5916cdf8c07f985b
TahBaconSandwich <ericds92@gmail.com>
2016-01-24 21:16:04 -0700

    google-reverse-geocoder class update
    	-add url function
    		-move url creation into separate function
    		
    	-switch curl call over to ken's curl command

--- src/providers/google-reverse-geocoder.php
+++ src/providers/google-reverse-geocoder.php
@@ -4,12 +4,12 @@ namespace SearchApi\Providers;
 
 use SearchApi\Models as Models;
 use SearchApi\Support as Support;
-use SearchApi\Test\Support\Mocks as Mocks;
+use SearchApi\Commands as Commands;
 use SearchApi\Services\ReverseGeocoder as ReverseGeocoder;
 
 /**
- * Class GoogleReverseGeocoder - Given a latlin or place id to find a point or polygon that can represent that
- * location via GoogleMaps.
+ * Class GoogleReverseGeocoder - Given a latlin or place id to find a point 
+ * or polygon that can represent that location via GoogleMaps.
  */
 class GoogleReverseGeocoder implements ReverseGeocoder {
   public $api_key; // service key
@@ -20,38 +20,49 @@ class GoogleReverseGeocoder implements ReverseGeocoder {
 
     if ( $parser === null ) {
       // default parser
-      $parser = new Support\GeoParser;
+      $parser = new Support\GeoParser();
     }
     $this->geo_parser = $parser;
   }
 
-  // returns array of terms based on latitude and longitude
-  public function get_locations( Models\GeoCoordinate $geo_coordinate ) {
-    // code to call the google geocoder service
-    // creating base url
+/**
+ * function for generating the url for the curl call
+ */
+  public function create_URL( Models\GeoCoordinate $geo_coordinate ) {
     $service_url = 'https://maps.googleapis.com/maps/api/geocode/json?'.
     "latlng={$geo_coordinate->lat},{$geo_coordinate->lng}";
 
     // checking if api key is given, if so adding it to url
-    // second half of if statement is for testing purposes
+    // SECOND PART OF IF: is meant for testing purposes
     if ( $this->api_key !== null && $this->api_key !== 'geokey' ) {
-      $service_url = $service_url.'&key={$this->api_key}';
+      $service_url = $service_url."&key={$this->api_key}";
     }
+    
+    return $service_url;
+  }
 
-    // implementing a curl call to the service
-    $curl = curl_init( $service_url );
-    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-    $geocoding_results = curl_exec( $curl );
+  /**
+   * function to gather the location data for the coordinates given
+   *
+   * @param Models\GeoCoordinate $geo_coordinate
+   * @throws Exception - error in curl call
+   */
+  public function get_locations( Models\GeoCoordinate $geo_coordinate ) {
+
+    //building the url
+    $service_url = create_URL();
+
+    // implementing a curl call using http-get curl call
+    $curl = new Commands\HttpGet();
+    $this->curl->setUrl($service_url);
+    $geocoding_results = $this->curl->execute();
 
     // checking if the curl was successful
     if ( $geocoding_results === false ) {
       $info = curl_getinfo( $curl );
-      curl_close( $curl );
       throw new Exception( 'error occured during curl exec. Additioanl info: ' . var_export( $info ) );
     }
-
-    // closing the curl
-    curl_close( $curl );
+    // end of the implementing a curl call using http-get curl call
 
     // calling json decoder
     $decoded_json = $this->geo_parser->reverse_geocoder_json_decoder( $geocoding_results );
