447b20977fb5c942296adf589658725787556552
TahBaconSandwich <ericds92@gmail.com>
2016-01-30 16:34:14 -0700

    update GeoCoderURLBuilderSpec
    	-add test for returning a string without selected builder
    	-add test for returning a string with google builder selected

--- test/spec/SearchApi/Support/GeoCoderURLBuilderSpec.php
+++ test/spec/SearchApi/Support/GeoCoderURLBuilderSpec.php
@@ -15,31 +15,19 @@ use Prophecy\Argument;
  */
 class GeoCoderURLBuilderSpec extends ObjectBehavior {
   function it_is_initializable() {
+    $this->beConstructedWith( new Models\GeoCoordinate( 40.714224, -73.961452 ) );
     $this->shouldHaveType( 'SearchApi\Providers\GeoCoderURLBuilder' );
   }
 
-  function it_shouldnt_have_an_api_key_by_default() {
-    $this->api_key->shouldBe( null );
+  function it_should_return_a_result_without_picked_url() {
+    $this->beConstructedWith( new Models\GeoCoordinate( 40.714224, -73.961452 ) );
+    $this->Url_Selector( null )
+    ->shouldHaveType('String');
   }
 
-  function it_should_return_a_result_without_key() {
-    $this->beConstructedWith( null, new Support\GeoParser );
-    $geo_cordinate = new Models\GeoCoordinate( 40.714224, -73.961452 );
-    $this->get_locations( $geo_cordinate )
-    ->shouldBeArray();
-  }
-
-  function it_should_return_a_result_with_key() {
-    $this->beConstructedWith( 'geokey', new Support\GeoParser );
-    $geo_cordinate = new Models\GeoCoordinate( 40.714224, -73.961452 );
-    $this->get_locations( $geo_cordinate )
-    ->shouldBeArray();
-  }
-
-  function it_should_throw_invalid_key() {
-    $this->beConstructedWith( 'bad_key_test', new Support\GeoParser );
-    $geo_cordinate = new Models\GeoCoordinate( 40.714224, -73.961452 );
-    $this->shouldThrow( new \Exception( 'Invalid Key' ) )
-    ->during( 'get_locations', array( $geo_cordinate ) );
+  function it_should_return_a_result_with_google_url_picked() {
+    $this->beConstructedWith( new Models\GeoCoordinate( 40.714224, -73.961452 ) );
+    $this->get_locations( 'Google' )
+    ->shouldHaveType('String');
   }
 }
