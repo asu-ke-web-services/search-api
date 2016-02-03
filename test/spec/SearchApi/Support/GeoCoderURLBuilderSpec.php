8e056e0b87b161583798150b855da9065a9fedb2
TahBaconSandwich <ericds92@gmail.com>
2016-02-02 20:29:56 -0700

    move GeoCoordinate variables out of the constructors
    	move to new function to improve code readability:
    		-move to new function to initialize
    		-call function in the constructor
    			-just return the coordinates

--- test/spec/SearchApi/Support/GeoCoderURLBuilderSpec.php
+++ test/spec/SearchApi/Support/GeoCoderURLBuilderSpec.php
@@ -14,26 +14,30 @@ use Prophecy\Argument;
  * GeoCoderURLBuilderSpec - Spec integration test for the GeoCoderURLBuilder (higher level functions)
  */
 class GeoCoderURLBuilderSpec extends ObjectBehavior {
+  function good_coord() {
+    return new Models\GeoCoordinate( 40.714224, -73.961452 );
+  }
+
   function it_is_initializable() {
-    $this->beConstructedWith( new Models\GeoCoordinate( 40.714224, -73.961452 ) );
+    $this->beConstructedWith( $this->good_coord() );
     $this->shouldHaveType( 'SearchApi\Support\GeoCoderURLBuilder' );
   }
 
   function it_should_return_a_result_without_picked_url() {
-    $this->beConstructedWith( new Models\GeoCoordinate( 40.714224, -73.961452 ) );
+    $this->beConstructedWith( $this->good_coord() );
     $this->url_selector( null )
     ->shouldBeString();
   }
 
   function it_should_return_a_result_with_google_url_picked() {
-    $this->beConstructedWith( new Models\GeoCoordinate( 40.714224, -73.961452 ) );
+    $this->beConstructedWith( $this->good_coord() );
     $this->url_selector( 'Google' )
     ->shouldBeString();
   }
 
   // specific builder tests
   function it_should_return_a_string_from_the_google_builder() {
-    $this-> beConstructedWith( new Models\GeoCoordinate( 40.714224, -73.961452 ) );
+    $this-> beConstructedWith( $this->good_coord() );
     $this->google_url()->shouldBeString();
   }
   // end of builder tests
