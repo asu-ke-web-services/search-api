eaf80df9e29bb561bda7f83bbb3c8c9177bdaac1
Ken Price <kaprice1@asu.edu>
2016-02-05 17:19:41 -0700

    phpdoc-style comments and unambiguous var names

--- src/builders/solr-query-builder.php
+++ src/builders/solr-query-builder.php
@@ -9,14 +9,18 @@ use SearchApi\Builders\QueryBuilder;
  */
 class SolrQueryBuilder implements QueryBuilder {
   /**
-   * Build a query string from provided search terms and search options
+   * Build a query string from provided search terms and search options.
+   * Returns a query string for a SOLR server.
+   *
+   * @param SearchTerm[]|null $searchTerms Search terms to search
+   * @param SearchApi\Models\SearchOptions|null $options Query options
    */
-  function build( $keywords, $options = null ) {
+  function build( $searchTerms, $options = null ) {
     $keywordStrings = '';
 
     error_log( var_dump( $options ) );
 
-    foreach ( $keywords as &$word ) {
+    foreach ( $searchTerms as &$word ) {
       $keywordStrings = $keywordStrings . $word->value . '%20';
     }
 
