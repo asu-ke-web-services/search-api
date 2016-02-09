c6b6e13f4b1cf9266415f7db3856e04ccd72d9c0
Jillian Karner <jillian.karner@gmail.com>
2016-02-09 13:52:47 -0700

    add example spec test that should succeed

--- test/spec/SearchApi/SearchEngineSpec.php
+++ test/spec/SearchApi/SearchEngineSpec.php
@@ -24,4 +24,16 @@ class SearchEngineSpec extends ObjectBehavior {
     $this->handle_request( $empty_search_request )->shouldReturnAnInstanceOf( 'SearchApi\Models\SearchResult' );
   }
 
+  function it_should_return_results_when_searching_for_foo( Search $search, Tagger $tagger, ReverseGeocoder $geocder) {
+    $this->beConstructedWith( $search, $tagger, $geocoder );
+
+    $foo_request = new Models\SearchRequest();
+    $foo_request->document = 'arizona arid phoenix';
+    $foo_request->text = 'asu school'
+
+    $response = $this->handle_request( $foo_request );
+    $response->results->shouldBeArray();
+    $response->results->shouldHaveCount( 1 );
+  }
+
 }
