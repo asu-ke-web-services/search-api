edfd79520e25b887f52a48144e36bb4060bc6b4a
Ken Price <kaprice1@asu.edu>
2016-02-23 19:05:23 -0700

    CamelCase to snake_case all the things.

--- src/models/search-options.php
+++ src/models/search-options.php
@@ -6,10 +6,10 @@ namespace SearchApi\Models;
  * Class SearchOptions - Contains search options available to user.
  * TODO: Sorting and filtering options.
  *
- * @var $resultsPerPage int|null   Number of results per page, null for default
+ * @var $results_per_page int|null   Number of results per page, null for default
  * @var $page           int|null   Which page to retrieve, null for default
  */
 class SearchOptions {
-  public $resultsPerPage = null;
+  public $results_per_page = null;
   public $page = null;
 }
