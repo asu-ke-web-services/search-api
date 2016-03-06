ede926b8ead87393ed5d192921e5d03af2059579
TahBaconSandwich <ericds92@gmail.com>
2016-03-06 11:58:15 -0700

    fix whitespace issues

--- src/providers/google-reverse-geocoder.php
+++ src/providers/google-reverse-geocoder.php
@@ -7,18 +7,16 @@ use SearchApi\Support as Support;
 use SearchApi\Commands as Commands;
 use SearchApi\Services\ReverseGeocoder as ReverseGeocoder;
 
-//TODO: add error exception that if hit then end of the url builders say fetch failed
-
 /**
  * Class ReverseGeocoder - Given a latlin or place id to find a point
  * or polygon that can represent that location via GoogleMaps.
  */
-class Google_Reverse_Geocoder implements ReverseGeocoder {
+class GoogleReverseGeocoder implements ReverseGeocoder {
   private $geo_coords;
 
   public function get_url( Models\GeoCoordinate $coords ) {
-  	// building the url
-  	$url_builder = new Support\Google_URLBuilder( $coords );
+    // building the url
+    $url_builder = new Support\Google_URLBuilder( $coords );
     return $url_builder->google_url();
   }
 
@@ -38,11 +36,11 @@ class Google_Reverse_Geocoder implements ReverseGeocoder {
 
     // attempting to call google's service
     try {
-    $curl_caller->setUrl( $url_builder->google_url() );
-    $geocoding_results = $curl_caller->execute();
-    // informing that the service failed is down
-    } catch (Exception $e) {
-      throw new Exception ( 'The Google Service is Unavailable:\n\t{$e}' );
+      $curl_caller->setUrl( $url_builder->google_url() );
+      $geocoding_results = $curl_caller->execute();
+      // informing that the service failed is down
+    } catch ( Exception $e ) {
+      throw new Exception( 'The Google Service is Unavailable:\n\t{$e}' );
     }
 
     // calling json decoder
--- src/services/reverse-geocoder.php
+++ src/services/reverse-geocoder.php
@@ -5,6 +5,6 @@ namespace SearchApi\Services;
 use SearchApi\Models as Models;
 
 interface ReverseGeocoder {
-	function get_url( Models\GeoCoordinate $coords );
+  function get_url( Models\GeoCoordinate $coords );
   function get_locations( Models\GeoCoordinate $geo_coordinate );
 }
