01fd9787c24b5f6f1fe659b3f5dbfba0449f7e96
Ken Price <kaprice1@asu.edu>
2016-03-21 00:34:42 -0700

    Implement POS Tagger; have it return all unique nouns tagged

--- src/providers/pos-tagger.php
+++ src/providers/pos-tagger.php
@@ -25,9 +25,44 @@ class PosTagger implements Tagger {
     );
   }
 
-  function tagger_service ( $request_string = '' ) {
+  function tagger_service( $request_string = '' ) {
+    $tagger_results = $this->pos->tag( explode( ' ', $request_string ) );
 
-    $result = $this->pos->tag(explode(' ', $request_string));
-    return $result;
+    // Return null if there are no results
+    if ( empty( $tagger_results ) ) {
+      return null;
+    }
+
+    // Output of pos-> tag is a nested array. Output is split by sentences, so let's
+    // flatten the array because we only care about the tagged words, not the sentences.
+    $flattened_results = array();
+    foreach ( $tagger_results as $result ) {
+      $flattened_results = array_merge( $flattened_results, $result );
+    }
+    // Next, let's make a list of unique words found.
+    $found_words = array();
+    array_walk_recursive( $tagger_results, function( $a ) use ( &$found_words ) {
+          $found_words[] = $a;
+    } );
+    $found_words = array_unique( $found_words );
+
+    // Now, let's use the list of unique words to build a new array with results for unique words.
+    // While we're at it, let's filter by noun phrases too (NN*)
+    $tagged_words = array();
+    foreach ( $found_words as $word ) {
+      foreach ( $flattened_results as $tagged_word ) {
+        if ( $tagged_word[0] === $word && strpos( $tagged_word[1], 'NN' ) !== false ) {
+          $tagged_words[] = array( $word, $tagged_word[1] );
+        }
+      }
+    }
+
+    $keywords = array();
+
+    foreach ( $tagged_words as $result ) {
+      array_push( $keywords, new Keyword( $result[0], 'NOUN', null ) );
+    }
+
+    return $keywords;
   }
 }
