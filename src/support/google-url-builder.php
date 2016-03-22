0ac5347320d912b60d58d7dec1377fd0ce1f44b3
TahBaconSandwich <ericds92@gmail.com>
2016-03-22 14:18:45 -0700

    update google-url-builder to allow for updating coords
    	-add function set_coords(coords)
    	-update constructor to allow for optional coordinates

--- src/support/google-url-builder.php
+++ src/support/google-url-builder.php
@@ -14,7 +14,15 @@ class GoogleURLBuilder {
 
   private $geo_coordinate;
 
-  public function __construct( Models\GeoCoordinate $coordinate ) {
+  public function __construct( Models\GeoCoordinate $coordinate = null ) {
+  	if ( $coordinate ) {
+      $this->geo_coordinate = $coordinate;
+    } else {
+      $this->geo_coordinate = new Models\GeoCoordinate( 0, 0 );
+    }
+  }
+
+  public function set_coords( Models\GeoCoordinate $coordinate ) {
     $this->geo_coordinate = $coordinate;
   }
 
