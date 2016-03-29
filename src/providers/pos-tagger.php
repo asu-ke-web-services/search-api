89136d2b9ee0839645f3d18db1bea7bb67d7576b
Ken Price <kaprice1@asu.edu>
2016-03-28 19:05:15 -0700

    Fix reference errors for foreach

--- src/providers/pos-tagger.php
+++ src/providers/pos-tagger.php
@@ -9,6 +9,7 @@ use Nectary\Configuration as Configuration;
 /**
  * PosTagger - This is a tagger implementation that uses the Part-Of-Speech tagger and returns an array of keywords.
  *             This implementation will filter tagged words so only NOUNS are returned!
+ *
  * @seeAlso: Stanford Log-Linear POS Tagger http://nlp.stanford.edu/software/tagger.html
  */
 class PosTagger implements Tagger {
@@ -35,23 +36,25 @@ class PosTagger implements Tagger {
 
     // Output of pos-> tag is a nested array. Output is split by sentences, so let's
     // flatten the array because we only care about the tagged words, not the sentences.
+    // Each item in flattened_results is a tagged word of form ["WORD", "XX"] where XX is
+    // the type of word (like "NNP" for noun phrase)
     $flattened_results = array();
-    foreach ( $tagger_results as $result ) {
+    foreach ( $tagger_results as &$result ) {
       $flattened_results = array_merge( $flattened_results, $result );
     }
+
     // Next, let's make a list of unique words found.
     $found_words = array();
-    array_walk_recursive( $tagger_results, function( $a ) use ( &$found_words ) {
-        if ( strlen( $a ) > 1 )
-            $found_words[] = $a;
-    } );
+    foreach ( $flattened_results as &$tagged_word ) {
+      $found_words[] = $tagged_word[0];
+    }
     $found_words = array_unique( $found_words );
 
     // Now, let's use the list of unique words to build a new array with results for unique words.
     // While we're at it, let's filter by noun phrases too (NN*)
     $tagged_words = array();
-    foreach ( $found_words as $word ) {
-      foreach ( $flattened_results as $tagged_word ) {
+    foreach ( $found_words as &$word ) {
+      foreach ( $flattened_results as &$tagged_word ) {
         if ( $tagged_word[0] === $word && strpos( $tagged_word[1], 'NN' ) !== false ) {
           $tagged_words[] = array( $word, $tagged_word[1] );
         }
